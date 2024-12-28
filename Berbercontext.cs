4e909a3527d420b18c9e3c5c23b0b6c4b6702f4a
Yunus Emre Yılmaz <103857600+Yunus-5874@users.noreply.github.com>
2024-12-28 19:36:46 +0300

    Add files via upload

---
+++ Berbercontext.cs
@@ -0,0 +1,18 @@
+﻿using BerberWebSitesi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BerberWebSitesi.Data
+{
+    public class BerberContext : DbContext
+    {
+        public BerberContext(DbContextOptions<BerberContext> options) : base(options) { }
+
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Barber> Barbers { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Service> Services { get; set; }
+
+    }
+}
