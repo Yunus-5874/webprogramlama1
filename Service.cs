0de142f4d6e92c389f62b35f63f40e9d087c941a
Yunus Emre Yılmaz <103857600+Yunus-5874@users.noreply.github.com>
2024-12-28 19:41:46 +0300

    Add files via upload

---
+++ Service.cs
@@ -0,0 +1,17 @@
+﻿// Service.cs
+using System.ComponentModel.DataAnnotations;
+
+namespace BerberWebSitesi.Models
+{
+    public class Service
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+        public TimeSpan Duration { get; set; }
+    }
+}
