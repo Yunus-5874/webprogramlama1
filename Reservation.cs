2b5e6c6e69d6b914ce395dd5cf8f990934178d52
Yunus Emre Yılmaz <103857600+Yunus-5874@users.noreply.github.com>
2024-12-28 19:41:13 +0300

    Add files via upload

---
+++ Reservation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BerberWebSitesi.Models
+{
+    public class Reservation
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("CustomerId")]
+        public Customer Customer { get; set; }
+
+        [ForeignKey("BarberId")]
+        public Barber Barber { get; set; }
+
+        [Required(ErrorMessage = "Randevu tarihi zorunludur.")]
+        public DateTime ReservationDate { get; set; }
+
+        [Required(ErrorMessage = "Hizmet türü zorunludur.")]
+        [MaxLength(200)]
+        public string ServiceType { get; set; }
+    }
+}
