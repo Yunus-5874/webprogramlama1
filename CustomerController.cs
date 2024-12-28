7acb7576692ceb79de48e21358a58463b2023153
Yunus Emre Yılmaz <103857600+Yunus-5874@users.noreply.github.com>
2024-12-28 19:35:12 +0300

    Add files via upload

---
+++ CustomerController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BerberWebSitesi.Data;
+using BerberWebSitesi.Models;
+
+namespace BerberWebSitesi.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly BerberContext _context;
+
+        public CustomerController(BerberContext context)
+        {
+            _context = context;
+        }
+
+        // Müşteri Ol formunu göster
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // Müşteri bilgilerini kaydet
+        [HttpPost]
+        public IActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Customers.Add(customer);
+                _context.SaveChanges();
+                return RedirectToAction("Success");
+            }
+
+            return View(customer);
+        }
+
+        // Başarılı kayıt sonrası mesaj
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
