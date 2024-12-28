1c9990f37211f880ef210dfc90031c454e79f9eb
Yunus Emre Yılmaz <103857600+Yunus-5874@users.noreply.github.com>
2024-12-28 19:36:16 +0300

    Add files via upload

---
+++ EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BerberWebSitesi.Data;
+using BerberWebSitesi.Models;
+using System.Linq;
+
+namespace BerberWebSitesi.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly BerberContext _context;
+
+        public EmployeeController(BerberContext context)
+        {
+            _context = context;
+        }
+
+        // Çalışan listeleme
+        public IActionResult Index()
+        {
+            var employees = _context.Employees.ToList();
+            return View(employees);
+        }
+    }
+}
