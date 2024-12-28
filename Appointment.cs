b46ff2585e41ad26ed1b904c7c542b05943a72bf
Yunus Emre Yılmaz <103857600+Yunus-5874@users.noreply.github.com>
2024-12-28 19:39:24 +0300

    Add files via upload

---
+++ Appointment.cs
@@ -0,0 +1,28 @@
+﻿// Appointment.cs
+using System.ComponentModel.DataAnnotations;
+
+namespace BerberWebSitesi.Models
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+        [Required]
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+
+        [Required]
+        public int ServiceId { get; set; }
+        public Service Service { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public decimal Price { get; set; }
+    }
+}
