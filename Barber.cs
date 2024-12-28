372022ed2930d35a479fd1bacaebe014918cfb44
Yunus Emre Yılmaz <103857600+Yunus-5874@users.noreply.github.com>
2024-12-28 19:39:56 +0300

    Add files via upload

---
+++ Barber.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BerberWebSitesi.Models
+{
+    public class Barber
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "İsim zorunludur.")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Uzmanlık alanı zorunludur.")]
+        public string Specialty { get; set; }
+
+        [Required(ErrorMessage = "Çalışma saatleri zorunludur.")]
+        public string WorkingHours { get; set; }
+    }
+}
