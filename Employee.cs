486bb53e2a475c6a4bbf37db7c1d67899c7a248b
Yunus Emre Yılmaz <103857600+Yunus-5874@users.noreply.github.com>
2024-12-28 19:40:42 +0300

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BerberWebSitesi.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Phone]
+        public string Phone { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string JobTitle { get; set; }
+
+        [Required]
+        public string WorkDays { get; set; }
+
+        [Required]
+        public string WorkHours { get; set; }
+
+        public string LeaveDays { get; set; }
+    }
+}
